2106bcd3b1d9b2e0621ab22c9c67c22827028df1
aorith <aomanu@gmail.com>
2019-04-03 20:27:23 +0200

    check every 12h

--- bin/duck.sh
+++ bin/duck.sh
@@ -1,13 +1,40 @@
-TOKEN="$(sed -n '1p' /home/aorith/secret/duckdns.txt)"
-DOM1="$(sed -n '2p' /home/aorith/secret/duckdns.txt)"
-DOM2="$(sed -n '3p' /home/aorith/secret/duckdns.txt)"
+#!/bin/bash
+
+TOKEN=$(sed -n '1p' "$HOME/secret/duckdns.txt")
+DOM1=$(sed -n '2p' "$HOME/secret/duckdns.txt")
+DOM2=$(sed -n '3p' "$HOME/secret/duckdns.txt")
 
 output=$(curl -k -s "https://www.duckdns.org/update?domains=$DOM1&token=$TOKEN&ip=")
-if [ "$output" != "OK" ]; then
-    /home/aorith/bin/telmsg.sh "$(date +'%Y%m%d %H:%M:%S') -> duckdns failed on $DOM1"
+if [ "$output" != "OK" ]
+then
+    # if flag exist and is older than seconds below, send alert
+    if [ -e "/tmp/fail-$DOM1" ]
+    then
+        if [ "$(( $(date +"%s") - $(stat -c "%Y" "/tmp/fail-$DOM1") ))" -gt "43200" ]
+        then
+            /home/aorith/bin/telmsg.sh "$(date +'%Y%m%d %H:%M:%S') -> duckdns failed on $DOM1"
+        fi
+    else
+        touch "/tmp/fail-$DOM1"
+    fi
+else
+    rm -f "/tmp/fail-$DOM1"
 fi
 
 output=$(curl -k -s "https://www.duckdns.org/update?domains=$DOM2&token=$TOKEN&ip=")
-if [ "$output" != "OK" ]; then
-    /home/aorith/bin/telmsg.sh "$(date +'%Y%m%d %H:%M:%S') -> duckdns failed on $DOM2"
+if [ "$output" != "OK" ]
+then
+    # if flag exist and is older than seconds below, send alert
+    if [ -e "/tmp/fail-$DOM2" ]
+    then
+        if [ "$(( $(date +"%s") - $(stat -c "%Y" "/tmp/fail-$DOM2") ))" -gt "43200" ]
+        then
+            /home/aorith/bin/telmsg.sh "$(date +'%Y%m%d %H:%M:%S') -> duckdns failed on $DOM2"
+        fi
+    else
+        touch "/tmp/fail-$DOM2"
+    fi
+else
+    rm -f "/tmp/fail-$DOM2"
 fi
+
