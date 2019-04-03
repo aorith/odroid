445c3f40e2554ee104d5d2af8dc0229ce05530de
aorith <aomanu@gmail.com>
2019-04-03 20:27:32 +0200

    fixes

--- bin/backups_clean.sh
+++ bin/backups_clean.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 BACKUP_PATH="/media/datos/backup"
-cd $BACKUP_PATH
+cd "$BACKUP_PATH" || exit 1
 
 #tempsensor
-while [ `ls $BACKUP_PATH/tempsensor/|wc -l` -ge 40 ]; do
-	f=`ls -1t $BACKUP_PATH/tempsensor/ | tail -1`
-	rm -v $BACKUP_PATH/tempsensor/$f
+while [ "$(find "$BACKUP_PATH/tempsensor/" | wc -l)" -ge 40 ]; do
+	f=$(find "$BACKUP_PATH/tempsensor/" | tail -1)
+	rm -v "$BACKUP_PATH/tempsensor/$f"
 	sleep 1
 done
