56d132ef49c90cb8d3171ca5eabf3b73265e04ff
aorith <aomanu@gmail.com>
2020-01-24 00:15:55 +0100

    sim

--- bin/vncmng.sh
+++ bin/vncmng.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 if [ "$1" = "start" ]; then
-    x11vnc -create -env FD_PROG=/usr/bin/fluxbox \
+    ( x11vnc -create -env FD_PROG=/usr/bin/fluxbox \
 	    -env X11VNC_FINDDISPLAY_ALWAYS_FAILS=1 \
-        -env X11VNC_CREATE_GEOM=${1:-800x600x16} \
+        -env X11VNC_CREATE_GEOM="800x600x16" \
         -gone 'killall Xvfb' \
-        -bg -nopw >/dev/null 2>&1
+        -ncache 10 \
+        -bg -nopw ) >/dev/null 2>&1
 elif [ "$1" = "stop" ]; then
-    killall fluxbox >/dev/null 2>&1
-    killall x11vnc >/dev/null 2>&1
-    killall Xvfb >/dev/null 2>&1
+    killall fluxbox
+    killall x11vnc
+    killall Xvfb
 elif [ "$1" = "fake" ]; then
     export DISPLAY=:1
-    Xvfb :1 -screen 0 800x600x16 >/dev/null 2>&1
-    fluxbox  >/dev/null 2>&1
-    x11vnc -display :1 -bg -nopw >/dev/null 2>&1
+    ( Xvfb :1 -screen 0 800x600x16
+    fluxbox
+    x11vnc -display :1 -bg -nopw ) >/dev/null 2>&1
 fi
