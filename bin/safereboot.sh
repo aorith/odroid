437d5119fcc72c501bdeaae5de849f04db356b10
aorith <aomanu@gmail.com>
2020-02-03 22:15:27 +0100

    adds

--- bin/safereboot.sh
+++ bin/safereboot.sh
@@ -5,6 +5,10 @@ sudo systemctl stop tomcat
 sudo systemctl stop bitcoind
 sudo systemctl stop transmission-daemon
 sudo systemctl stop qbittorrent
+#cd /var/www/html/nextcloud
+#sudo -u www-data php occ maintenance:mode --on
+sudo systemctl stop mariadb
+sudo systemctl stop apache2
 sudo sync
 echo "Rebooting in 5..."
 sleep 5
