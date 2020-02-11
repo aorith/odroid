9d51b1c502bf64626425d264e1e8f70635ef08c7
Manuel Sanchez Pinar <aomanu@gmail.com>
2020-02-11 10:28:16 +0100

    small change due to geoiplookup ver

--- bin/geoipbanned.sh
+++ bin/geoipbanned.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 #IPLIST=$(grep -v "#" /etc/hosts.deny |grep "ALL:" |cut -d: -f2)
+#IPLIST=$(sudo iptables -L -n | awk '$1=="REJECT" && $4!="0.0.0.0/0"' | awk '{ print $4 }')
 IPLIST=$(sudo iptables -L -n | awk '$1=="REJECT" && $4!="0.0.0.0/0"' | awk '{ print $4 }')
+# TBD:
+# sudo nft list set ip fail2ban f2b-sshd
+# sudo nft list set ip fail2ban f2b-apache-custom
 for i in $(echo "$IPLIST")
 do
     echo -n "$i - "
-    geoiplookup "$i" | cut -d':' -f 2
+    geoiplookup "$i" | head -1 | cut -d':' -f 2
 done
 
 echo "Total: $(echo "$IPLIST" | tr ' ' '\n'| wc -l)"
