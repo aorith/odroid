ab6ad31d5fd15f88f05ba925a1bff0a3c22d63eb
Manuel Sanchez Pinar <aomanu@gmail.com>
2020-05-10 20:15:27 +0200

    cleanup

--- tempsensor/update_graphs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")" || exit 1
-if [ -e /tmp/update_graphs ]
-then
-    ./graphs/graphs.py
-    /home/aorith/githome/odroid/bin/telmsg.sh "graphs updated"
-    sudo rm /tmp/update_graphs
-fi
